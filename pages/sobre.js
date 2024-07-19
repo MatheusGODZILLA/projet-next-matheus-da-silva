3cf2535b460e7356f320be0715c2e0ec9d355715
Matheus da Silva <contagodzillajapan@gmail.com>
2024-07-18 23:16:37 -0300

    Criando estrutura da página HQ

---
+++ pages/sobre.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import SobreSection from '../components/SobreSection';
+
+export default function Sobre() {
+  const [metadata, setMetadata] = useState({ title: '', description: '' });
+
+  useEffect(() => {
+    fetch('/api/page-metadata?page=sobre')
+      .then((res) => res.json())
+      .then((data) => setMetadata(data));
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <SobreSection />
+      <Footer />
+    </>
+  );
+}
